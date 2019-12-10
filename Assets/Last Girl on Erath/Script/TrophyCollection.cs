3731a5bcc2ac7f44b3e5b7babf5c7fcac957dfc5
stibaer <stibaer@gmail.com>
2019-12-10 14:45:11 +0100

    Update to Unity x.15
    done: Updated to Unity x.15

--- Assets/Last Girl on Erath/Script/TrophyCollection.cs
+++ Assets/Last Girl on Erath/Script/TrophyCollection.cs
@@ -7,14 +7,6 @@ public class TrophyCollection : MonoBehaviour
 
 
     public bool collect = false;
-<<<<<<< HEAD
-    Trophyscript ontrophy;  
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        ontrophy = GameObject.FindGameObjectWithTag("Item").GetComponent<Trophyscript>();
-=======
     Trophyscript trophy;
     public SpriteRenderer spriteRenderer;
 
@@ -22,24 +14,16 @@ public class TrophyCollection : MonoBehaviour
     void Start()
     {
         trophy = GameObject.FindGameObjectWithTag("Item").GetComponent<Trophyscript>();
->>>>>>> Initial Commit
     }
 
     // Update is called once per frame
     void Update()
     {
-<<<<<<< HEAD
-        if (ontrophy) 
-        {
-            
-            gameObject.SetActive(true);
-=======
         if (trophy.ontrophy) 
         {
 
             this.spriteRenderer = GetComponent<SpriteRenderer>();
             this.spriteRenderer.enabled = true;
->>>>>>> Initial Commit
         }
     }
 }
