348385829723afdcaae120e0d2f6d2c50b6ad0ec
IroK <jlaboga@gmail.com>
2019-12-12 11:32:09 +0100

    Added ZeroFriction Material to Cars, Enemy & Mülltonne Prefab

---
+++ Assets/Scripts/Enemy/EnemyTestHealth.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTestHealth : MonoBehaviour
+{
+    public float health = 1;
+    void ApplyDamage(float damage)
+    {
+        health -= damage;
+        if (health <= 0)
+            Destroy(gameObject);
+    }
+
+}
