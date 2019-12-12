6739f6d5976038c91684bfedf63692b427d571d0
IroK <jlaboga@gmail.com>
2019-12-12 13:26:02 +0100

    meta

--- Assets/Scripts/Character/Ashley.cs
+++ Assets/Scripts/Character/Ashley.cs
@@ -114,7 +114,7 @@ public class Ashley : MonoBehaviour
                                             m_grounded = false;
                                             m_animator.SetBool("Grounded", m_grounded);
                                             m_body2d.velocity = new Vector2(m_body2d.velocity.x, m_jumpForce);
-                                            m_groundSensor.Disable(0.2f);
+                                            m_groundSensor.Disable(0.1f);
                                         }
 
                                         //Run
