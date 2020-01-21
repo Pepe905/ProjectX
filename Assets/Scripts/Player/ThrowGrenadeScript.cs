de251d7473ee0decc06f45dc7e5bb033a7031597
stibaer <stibaer@gmail.com>
2020-01-21 22:49:44 +0100

    Grenade (Sript + Prefab)
    Simply put the GrenadeScript on the PlayerPrefab, and select the GrenadePrefab and the hand to throw the grenade with.
    Todo: Add cooldown time or limited stock that can be refilled usign pickups to this ability to prevent spamming.
    ParticleEffect not included yet. Have to clean it up first.

---
+++ Assets/Scripts/Player/ThrowGrenadeScript.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowGrenadeScript : MonoBehaviour
+{
+
+    public GameObject grenadePrefab;
+    public Transform hand;
+    public float throwForce = 10f;
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            GameObject gren = Instantiate(grenadePrefab, hand.position, hand.rotation) as GameObject;
+            gren.GetComponent<Rigidbody>().AddForce(hand.forward * throwForce, ForceMode.Impulse);
+
+
+        }
+    }
+}
---
+++ Assets/Scripts/Weapons/Grenade/GrenadeScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrenadeScript : MonoBehaviour
+{
+    public float timeDelay = 2f;
+    float startTimer;
+
+    public int damage = 10;
+    public float explosiveForce = 20f;
+    public float explosiveRadius = 15f;
+
+
+    [SerializeField] GameObject exploParticle;
+
+
+    void Update() {
+        startTimer += Time.deltaTime;
+        if (startTimer >= timeDelay) { 
+            Explode(); //Function explode get called
+        }
+    }
+
+    void Explode() // Function that gets called
+    {
+        Collider[] coll = Physics.OverlapSphere(transform.position, explosiveRadius);
+
+        for (int i = 0; i < coll.Length; i++) {
+            if (coll [i].gameObject.GetComponent <TestDummy> ()) {
+                coll[i].gameObject.GetComponent<TestDummy>().TakeDamage(damage);
+                coll[i].gameObject.GetComponent<Rigidbody>().AddExplosionForce(explosiveForce, transform.position, explosiveRadius);
+            }
+        }
+
+        
+        GameObject spawnedParticle = Instantiate(exploParticle, transform.position, transform.rotation);
+        Destroy(spawnedParticle, 5);
+        /*
+        Collider[] colliders = Physics.OverlapSphere(transform.position, radius);
+        foreach (Collider nearbyObject in colliders)
+        {
+            Rigidbody rb = nearbyObject.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                rb.AddExplosionForce(force, transform.position, radius);
+            }
+        }
+        
+            hasExploded = true;
+            */
+        Destroy(gameObject);
+    }
+}
