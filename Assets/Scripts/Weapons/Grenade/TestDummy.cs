0ec22a8368b699e07f6a18f4aa2a494891df5063
DragonFiighterLI <Elisabeth.kaske@gmx.de>
2020-01-22 10:18:32 +0100

    Sti: Grenade: Fixed compiler error
    TestDumym fiel was missing. Coudlnt see this at home, since its locally available there.

---
+++ Assets/Scripts/Weapons/Grenade/TestDummy.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestDummy : MonoBehaviour {
+
+
+    public void TakeDamage(int amn) {
+        Debug.Log("enemy took damage " + amn.ToString());
+    }
+
+}
