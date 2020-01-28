674af0708ba97b3f7923c84c8a7eb1652769c830
stibaer <stibaer@gmail.com>
2020-01-28 14:16:16 +0100

    Checkpoint: SpriteExchangeScript
    Script exchanges the sprite of the cehckpoint when touched by player or object tagged as player

---
+++ Assets/Scripts/Features/SpriteExchangeScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteExchangeScript : MonoBehaviour
+{
+
+    public int itemType;
+    public bool checkpointActive = false;
+    public SpriteRenderer targetSprite;
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+    }
+
+
+        void OnTriggerEnter2D(Collider2D other)
+        {
+            // Player
+            if (other.gameObject.tag == "Player")
+            {
+                checkpointActive = true; 
+                Debug.Log("Checkpoint activated by player");
+            //Item
+                gameObject.SetActive(false);
+                
+
+
+
+            }
+            
+        }
+    
+}
