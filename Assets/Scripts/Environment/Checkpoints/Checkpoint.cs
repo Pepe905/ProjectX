9ce34f7f6b02fdeee42d35ac57e9d68c6db57b26
stibaer <stibaer@gmail.com>
2020-01-28 13:00:11 +0100

    Checkpoint
    Exchanged old sprite for new one. Made some adaptions.

--- Assets/Scripts/Environment/Checkpoints/Checkpoint.cs
+++ Assets/Scripts/Environment/Checkpoints/Checkpoint.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class Checkpoint : MonoBehaviour {
 	
 	private GameMaster gm;
-	
+	//[SerializeField] private GameObject disappearParticle;
+
 	void Start(){
 		gm = GameObject.FindGameObjectWithTag("GM").GetComponent<GameMaster>();
 		
@@ -13,7 +14,13 @@ public class Checkpoint : MonoBehaviour {
 	
 	void OnTriggerEnter2D(Collider2D other){
 		if(other.CompareTag("Player")){
-			gm.lastCheckPointPos = transform.position;	
+			/*
+			GameObject spawnedParticle = Instantiate(disappearParticle, transform.position, transform.rotation);
+			//spawnedParticle.transform.parent = transform.root;
+			Destroy(spawnedParticle, 5);
+			*/
+			gm.lastCheckPointPos = transform.position; //Was before above. Changed order for testing
+
 		}
     }
 }
