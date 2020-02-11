4024cadae522567690f4f4b321ab2f6b4afce5ff
stibaer <stibaer@gmail.com>
2020-02-11 23:24:53 +0100

    Fixed Checkpoints, improved player, fixed void links
    Had to add the PlayerPositionScript to the Player again, adapt values at mutiple spots.
    The palyers default looking direction is to the right. Needed adaptations in prefab done.
    Removed some links to particle effects that we wont use anymore.

--- Assets/Scripts/Environment/Checkpoints/Checkpoint.cs
+++ Assets/Scripts/Environment/Checkpoints/Checkpoint.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Checkpoint : MonoBehaviour {
 	
 	private GameMaster gm;
-	//[SerializeField] private GameObject disappearParticle;
+	[SerializeField] private GameObject disappearParticle;
 
 	void Start(){
 		gm = GameObject.FindGameObjectWithTag("GM").GetComponent<GameMaster>();
@@ -14,13 +14,11 @@ public class Checkpoint : MonoBehaviour {
 	
 	void OnTriggerEnter2D(Collider2D other){
 		if(other.CompareTag("Player")){
-			/*
+			gm.lastCheckPointPos = transform.position;
+			
 			GameObject spawnedParticle = Instantiate(disappearParticle, transform.position, transform.rotation);
-			//spawnedParticle.transform.parent = transform.root;
+			spawnedParticle.transform.parent = transform.root;
 			Destroy(spawnedParticle, 5);
-			*/
-			gm.lastCheckPointPos = transform.position; //Was before above. Changed order for testing
-
 		}
     }
 }
