18b3d7c667a0ecb8e1d45056c87717305a87d3d7
IroK <jlaboga@gmail.com>
2020-02-12 16:32:36 +0100

    Level, Intro, Menues

---
+++ Assets/Scripts/Character/WinScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinScript : MonoBehaviour
+{
+
+    public int itemType;
+    public bool onLiev = false;
+    public Transform targetTransform;
+    public string Scene;
+
+    public void SwitchScene()
+
+    {
+        SceneManager.LoadScene(Scene);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        // Player
+        if (other.gameObject.tag == "Player")
+        {
+            onLiev = true;
+            SceneManager.LoadScene(Scene);
+
+        }
+
+        
+        
+    }
+}
