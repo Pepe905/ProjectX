9e127a2c3eef72d0616e4139fcba3740cf854984
stibaer <stibaer@gmail.com>
2020-02-12 02:24:18 +0100

    ShootingSound stopping too early
    Approch as comment in the code with source.

--- Assets/Scripts/Character/Patrone.cs
+++ Assets/Scripts/Character/Patrone.cs
@@ -8,6 +8,7 @@ public class Patrone : MonoBehaviour
     public float damage = 1;
     private Vector3 startPoint;
     [SerializeField] private float range = 100;
+    //private AudioClip collectSound; https://answers.unity.com/questions/17856/sound-stops-when-object-is-destroyed.html
 
     public void Start()
     {
@@ -27,16 +28,8 @@ public class Patrone : MonoBehaviour
         if (other.CompareTag("Enemy"))
         {
             other.SendMessage("ApplyDamage", damage, SendMessageOptions.DontRequireReceiver);
+            //AudioSource.PlayClipAtPoint(collectSound, transform.position); https://answers.unity.com/questions/17856/sound-stops-when-object-is-destroyed.html
             Destroy(gameObject);
-
         }
     }
-
-
-
-
-
-
-
-
 }
