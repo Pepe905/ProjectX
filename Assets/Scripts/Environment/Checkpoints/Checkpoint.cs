1201931e05f61a9fc60c56039765dbd6918a8b22
stibaer <stibaer@gmail.com>
2020-02-12 03:20:00 +0100

    Checkpoint: Added SoundOnActivation
    ToDo: Only paly sound wneh checkpoint has not been activated.
    moved soem odler files to proper folders.

--- Assets/Scripts/Environment/Checkpoints/Checkpoint.cs
+++ Assets/Scripts/Environment/Checkpoints/Checkpoint.cs
@@ -3,22 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Checkpoint : MonoBehaviour {
-	
+
 	private GameMaster gm;
 	[SerializeField] private GameObject disappearParticle;
+
+	[SerializeField] private AudioClip checkpointActivated;
+	AudioSource checkpointAS;
+	//public float Volume;
+	//public bool alreadyActive = false;
 
 	void Start(){
 		gm = GameObject.FindGameObjectWithTag("GM").GetComponent<GameMaster>();
 		
+		checkpointAS = GetComponent<AudioSource>();
+
 	}
 	
 	void OnTriggerEnter2D(Collider2D other){
-		if(other.CompareTag("Player")){
+		if(other.CompareTag("Player")) {
 			gm.lastCheckPointPos = transform.position;
-			
+
+			checkpointAS.PlayOneShot(checkpointActivated);
+
 			GameObject spawnedParticle = Instantiate(disappearParticle, transform.position, transform.rotation);
 			spawnedParticle.transform.parent = transform.root;
 			Destroy(spawnedParticle, 5);
+
+			//checkpointAS.PlayOneShot(checkpointActivated, Volume);
+			//alreadyActive = true;
 		}
     }
 }
---
+++ Assets/Scripts/Environment/Checkpoints/CheckpointSoundScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointSoundScript : MonoBehaviour {
+    public AudioClip SoundToPlay;
+    public float Volume;
+    AudioSource audio;
+    public bool alreadyPlayed = false;
+    private void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter()
+    {
+        audio.PlayOneShot(SoundToPlay, Volume);
+        alreadyPlayed = true;
+    }
+
+}
