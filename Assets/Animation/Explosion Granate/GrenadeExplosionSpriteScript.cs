c253be4665789b10bff5501c214585d2a35232d7
stibaer <stibaer@gmail.com>
2020-02-12 12:22:50 +0100

    GrenadeSpriteExplosion
    Tobis sprite based grenade explosion now works and looks awesome! :)

---
+++ Assets/Animation/Explosion Granate/GrenadeExplosionSpriteScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrenadeExplosionSpriteScript : MonoBehaviour
+{
+    void Start()
+    {
+        Animator animator = GetComponent<Animator>();
+        animator.SetBool("isExploding", true);
+        Destroy(gameObject, animator.GetCurrentAnimatorStateInfo(0).length);
+    }
+}
